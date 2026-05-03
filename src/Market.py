66a629989f2845afec3d3cdbc8f1f2d2166cfd70
V4laky <lovas.lacko@gmail.com>
2026-05-03 13:37:11 +0200

    dynamic beta option

--- src/Market.py
+++ src/Market.py
@@ -8,16 +8,26 @@ def log_norm_params(mu, sig):
 
     return mu_norm, sig_norm
 
+def get_uncond_vol(market_garch, garch_scaling, shrink=0.9):
+    p = market_garch.params
+    uncond_var = p['omega']/(1-p['alpha[1]']*shrink - p['beta[1]']*shrink) # shrink parameters since a+b close to 1
+    return np.sqrt(uncond_var)/garch_scaling
+
+
 # Market return around 0.00028
 
 class Market():
     
-    def __init__(self, n_assets, n_sectors, market_model, rf = 0.0001, market_scale_arch=1):
+    def __init__(self, n_assets, n_sectors, market_model, rf = 0.0001, market_scale_arch=1, 
+                 dynamic_beta=True):
 
         np.random.seed(42)
 
         self.market_model = market_model
         self.scale = market_scale_arch
+
+        self.dynamic_beta = dynamic_beta
+        self.uncond_market_vol = get_uncond_vol(self.market_model, self.scale, 0.9)
 
         self.n_assets = n_assets
         self.n_sectors = n_sectors
@@ -26,7 +36,7 @@ class Market():
         self.alphas = np.random.normal(0, 0.0001, n_assets)
         self.betas = np.random.normal(1, 0.2, n_assets)
 
-        self.vols = np.random.lognormal(*log_norm_params(0.00945, 0.00315), n_assets)
+        self.vols = np.random.lognormal(*log_norm_params(0.00945, 0.00315), n_assets) # residaul vol
 
         self.sectors = np.random.randint(0, n_sectors, n_assets)
 
@@ -34,8 +44,8 @@ class Market():
         
         self.u = np.random.uniform(0.35, 0.45, n_assets) # The amount of variance explained by sectors in %
 
-        self.sector_coeffs = self.u * self.vols / self.sector_vols[self.sectors]
-        self.vols *= (1 - self.u) # remaining vol
+        self.sector_coeffs = np.sqrt(self.u) * (self.vols / self.sector_vols[self.sectors])
+        self.vols *= np.sqrt(1 - self.u) # remaining vol
     
     def summary(self):
         df = pd.DataFrame({'alpha': self.alphas, 'beta': self.betas, 'vols': self.vols,
@@ -57,7 +67,17 @@ class Market():
             sector_returns[i] = np.random.standard_t(5, nobs) * self.sector_vols[i] * np.sqrt((5-2)/5)
 
 
-        returns = self.alphas[:, None] + self.betas[:, None] @ (r_m.to_numpy()[None, :] - self.rf) + self.rf
+        if self.dynamic_beta:
+            scale = 1 + 0.1*(m_vol/self.uncond_market_vol - 1)
+            np.clip(scale, 1.0, 2.0) # lower 1.0 to not reduce betas
+            
+            betas = self.betas[:, None] @ (scale).to_numpy()[None, :]
+            
+            returns = self.alphas[:, None] + betas * (r_m.to_numpy()[None, :] - self.rf) + self.rf
+        else:
+            returns = self.alphas[:, None] + self.betas[:, None] @ (r_m.to_numpy()[None, :] - self.rf) + self.rf
+
+        
         returns += sector_returns[self.sectors] * self.sector_coeffs[:, None]
         returns += np.random.standard_normal((self.n_assets, nobs)) * self.vols[:, None]
 
